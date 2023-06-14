9932435854157cb3e6aa4a0535aa04ff161575a3
Allen Guan <me@nella17.tw>
2023-06-15 00:47:35 +0800

    [Huffman-Coding] fix exp.sh

--- Huffman-Coding/scripts/exp.sh
+++ Huffman-Coding/scripts/exp.sh
@@ -4,10 +4,10 @@ set -ux
 for i in {1..8}; do
   k="$((i * 8))"
   (name="basic.$k" ARGS="-t basic -b $k -v --no-time" ./scripts/test-report.sh > "log/exp-basic-$k.log" 2>&1)
-  (name="basic.$k.40MB" ARGS="-t basic -b $k -s 41943040 -v --no-time" ./chart-compression-ratio-split/test-report.sh > "log/exp-basic-$k-40MB.log" 2>&1)
+  (name="basic.$k.40MB" ARGS="-t basic -b $k -s 41943040 -v --no-time" ./test-report.sh > "log/exp-basic-$k-40MB.log" 2>&1)
 done
 
-(name="adaptive.8" ARGS="-t adaptive -b 8 -v --no-time" ./chart-compression-ratio-split/test-report.sh > "log/exp-adaptive-8.log" 2>&1)
+(name="adaptive.8" ARGS="-t adaptive -b 8 -v --no-time" ./test-report.sh > "log/exp-adaptive-8.log" 2>&1)
 
 time diff alexnet.pth <(./bin/release/huffman-coding.exe -t adaptive -v -e -i ./alexnet.pth --ostream | ./bin/release/huffman-coding.exe -t adaptive -v -d) && echo "success"
 
